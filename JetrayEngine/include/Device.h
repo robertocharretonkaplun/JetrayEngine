1b6b76d21662e6e6e2ff04583d85bf6b39699de1
Roberto Charreton <61713617+robertocharretonkaplun@users.noreply.github.com>
2024-01-29 10:36:30 -0600

    main - Device Methods and Creation of Device Context

--- JetrayEngine/include/Device.h
+++ JetrayEngine/include/Device.h
@@ -34,6 +34,35 @@ public:
 												 const D3D11_DEPTH_STENCIL_VIEW_DESC* pDesc,
 												 ID3D11DepthStencilView** ppDepthStencilView);
 
+	HRESULT
+	CreateVertexShader(const void* pShaderBytecode,
+										 unsigned int BytecodeLength,
+										 ID3D11ClassLinkage* pClassLinkage,
+										 ID3D11VertexShader** ppVertexShader);
+
+	HRESULT 
+	CreateInputLayout(const D3D11_INPUT_ELEMENT_DESC* pInputElementDescs,
+										unsigned int NumElements,
+										const void* pShaderBytecodeWithInputSignature,
+										unsigned int BytecodeLength,
+										ID3D11InputLayout** ppInputLayout);
+
+
+	HRESULT 
+	CreatePixelShader(const void* pShaderBytecode,
+										unsigned int BytecodeLength,
+										ID3D11ClassLinkage* pClassLinkage,
+										ID3D11PixelShader** ppPixelShader);
+
+	HRESULT 
+	CreateBuffer(const D3D11_BUFFER_DESC* pDesc,
+							 const D3D11_SUBRESOURCE_DATA* pInitialData,
+							 ID3D11Buffer** ppBuffer);
+
+	HRESULT 
+	CreateSamplerState(const D3D11_SAMPLER_DESC* pSamplerDesc,
+										 ID3D11SamplerState** ppSamplerState);
+
 public:
 	ID3D11Device* m_device = nullptr;
 };
